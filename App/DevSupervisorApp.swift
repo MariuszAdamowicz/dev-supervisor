b57af9a1f701a79ef248163a6a81983347c0e469
FlakeWars <flakewars@gmail.com>
2026-03-29 01:29:25 +0100

    chore: align project structure with playbook

---
+++ App/DevSupervisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevSupervisorApp.swift
+//  DevSupervisor
+//
+//  Created by Mariusz Adamowicz on 29/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct DevSupervisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
